8cd9ec29f609693bfdde1381b2659c97f4f59f02
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2024-06-04 15:26:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const LINKS = [
     "<a class='pages' href='https://destiny-puzzle-mapper-10258304.codehs.me/page4.html'>Page 4</a>",
     "<a class='pages' href='https://destiny-puzzle-mapper-10258304.codehs.me/page5.html'>Page 5</a>",]
 document.body.innerHTML += "<div class='search-container'><input type='text' id='searchInput' onkeyup='filterFunction()' onfocus='showDropdown()' placeholder='Search...'><div id='dropdown' class='dropdown-content'>"+LINKS.join("")+"</div></div>"
-document.body.innerHTML += "<div id='form'><p class='header'>Missing Puzzles/Secrets? Feature Ideas/Requests? Want to Help Work on the Site?</p><p>click <a style='color:red;' href=''./forms'>Here</a>, or Go to the Forms tab, or  go to <span style='color:red'>https://destiny-puzzle-mapper-10258304.codehs.me/forms</span></p></div>"
+// document.body.innerHTML += "<div id='form'><p class='header'>Missing Puzzles/Secrets? Feature Ideas/Requests? Want to Help Work on the Site?</p><p>click <a style='color:red;' href=''./forms'>Here</a>, or Go to the Forms tab, or  go to <span style='color:red'>https://destiny-puzzle-mapper-10258304.codehs.me/forms</span></p></div>"
 function filterFunction() {
     let input = document.getElementById('searchInput');
     let filter = input.value.toLowerCase();
